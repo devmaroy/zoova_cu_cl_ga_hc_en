67743eec17a562089e468dc0c52973890795242c
Marek Matejovic <me@devmaroy.cz>
2020-09-30 18:59:25 +0200

    Mobile hero

--- src/components/pages/hero.js
+++ src/components/pages/hero.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import HeroBg from '../../images/pages/hero/hero-bg-2.jpg';
 
 const Hero = () => {
   return (
-    <div className="hero">
-      <div className="container">
-        <div className="hero__inner">
-          <h1>Zoova Foundation</h1>
+    <div className="hero" style={{ backgroundImage: `url( ${HeroBg} )` }}>
+      <div className="hero__overlay">
+        <div className="hero__inner container">
+          <h1 className="hero__heading">Zoova Foundation</h1>
 
-          <p>A grateful heart is a beginning of greatness.</p>
+          <div className="hero__text">
+            <p>A grateful heart is a beginning of greatness.</p>
+          </div>
 
-          <Link to="/gallery" className="button button--outline">
+          <Link to="/" className="button button--outline">
             View gallery
           </Link>
-          <Link to="/gallery" className="button button--outline">
+          <Link to="/" className="button button--outline">
             View gallery
           </Link>
         </div>
