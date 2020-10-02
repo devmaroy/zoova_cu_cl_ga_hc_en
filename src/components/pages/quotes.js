7719a0dbc18a5403cfeaa63af6578ea87bf84e53
Marek Matejovic <me@devmaroy.cz>
2020-10-02 18:33:32 +0200

    Added margin for faq, added new css variable for color, removed comments

--- src/components/pages/quotes.js
+++ src/components/pages/quotes.js
@@ -24,30 +24,3 @@ const Quotes = () => {
 };
 
 export default Quotes;
-
-// import React from 'react';
-// import quotesBg from '../../images/pages/quotes/quotes-bg.jpg';
-
-// const Quotes = () => {
-//   return (
-//     <section
-//       className="section quotes"
-//       style={{ backgroundImage: `url( ${quotesBg} )` }}
-//     >
-//       <div className="quotes__overlay overlay">
-//         <div className="quotes__inner container">
-//           <blockquote className="quotes__text section__text">
-//             <p>
-//               This country will not be a good place for any of us to live in
-//               unless we make it a good place for all of us to live in.
-//             </p>
-
-//             <footer>—Theodore Roosevelt</footer>
-//           </blockquote>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default Quotes;
