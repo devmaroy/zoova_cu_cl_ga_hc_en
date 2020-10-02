cff46fc2ff8cccb51109e874acde1fce54bc41b1
Marek Matejovic <me@devmaroy.cz>
2020-10-02 18:29:50 +0200

    Accordion component done

--- src/components/common/accordion.js
+++ src/components/common/accordion.js
@@ -1,14 +1,39 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable react/no-danger */
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 const Accordion = ({ items }) => {
+  const [activePanels, setActivePanels] = useState({});
+
+  const revealText = (index) => {
+    setActivePanels((prevState) => ({
+      ...prevState,
+      [index]: !prevState[index],
+    }));
+  };
+
   return (
     <dl className="accordion">
-      {items.map(({ id, heading, text }) => (
+      {items.map(({ id, heading, text }, index) => (
         <React.Fragment key={id}>
-          <dt className="accordion__heading">{heading}</dt>
-          <dd className="accordion__text">{text}</dd>
+          <dt
+            className={classNames('accordion__heading', {
+              active: activePanels[index],
+            })}
+            onClick={() => revealText(index)}
+            onKeyDown={() => {}}
+          >
+            {heading}
+          </dt>
+          <dd
+            className={classNames('accordion__text', {
+              active: activePanels[index],
+            })}
+          >
+            {text}
+          </dd>
         </React.Fragment>
       ))}
     </dl>
