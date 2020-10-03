c9e23cb7baef7dc35b4f74dbb135d09fb85308f5
Marek Matejovic <me@devmaroy.cz>
2020-10-03 15:02:05 +0200

    Fixed logo

--- src/components/common/logo.js
+++ src/components/common/logo.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 const Logo = () => {
   return (
     <div className="logo">
-      <span className="logo__highlight">Zoova</span> Foundation
+      <Link to="/" className="logo__link">
+        <span className="logo__highlight">Zoova</span> Foundation
+      </Link>
     </div>
   );
 };
