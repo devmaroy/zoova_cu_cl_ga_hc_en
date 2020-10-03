03c5f09f466f88d77a4bf2f6f790524877e56708
Marek Matejovic <me@devmaroy.cz>
2020-10-03 17:08:35 +0200

    Footer section for tablets

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -7,23 +7,25 @@ const Footer = () => {
     <footer className="footer">
       <div className="container">
         <div className="footer__inner">
-          <div className="footer__copyright">
-            <p>
-              Copyright © {new Date().getFullYear()}{' '}
-              <Link to="/" className="footer__link">
-                Zoova
-              </Link>
-              . All rights reserved.
-            </p>
-          </div>
+          <div className="footer__info">
+            <div className="footer__copyright">
+              <p>
+                Copyright © {new Date().getFullYear()}{' '}
+                <Link to="/" className="footer__link">
+                  Zoova
+                </Link>
+                . All rights reserved.
+              </p>
+            </div>
 
-          <div className="footer__author">
-            <p>
-              Made with <span className="footer__highlight">love</span> by{' '}
-              <a href="https://marekmatejovic.com/" className="footer__link">
-                devmaroy
-              </a>
-            </p>
+            <div className="footer__author">
+              <p>
+                Made with <span className="footer__highlight">love</span> by{' '}
+                <a href="https://marekmatejovic.com/" className="footer__link">
+                  devmaroy
+                </a>
+              </p>
+            </div>
           </div>
 
           <div className="footer__social">
