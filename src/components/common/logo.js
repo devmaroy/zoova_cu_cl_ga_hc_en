77413e1adfbdb96e1ecead77f785c8450ad946fe
Marek Matejovic <me@devmaroy.cz>
2020-10-07 18:04:12 +0200

    Animation added for logo and navigation

--- src/components/common/logo.js
+++ src/components/common/logo.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { Tween } from 'react-gsap';
 
 const Logo = () => {
   return (
-    <div className="logo">
-      <Link to="/" className="logo__link">
-        <span className="logo__highlight">Zoova</span> Foundation
-      </Link>
-    </div>
+    <Tween from={{ y: '-50', opacity: 0 }} ease="power1.out">
+      <div className="logo">
+        <Link to="/" className="logo__link">
+          <span className="logo__highlight">Zoova</span> Foundation
+        </Link>
+      </div>
+    </Tween>
   );
 };
 
--- src/components/layout/navigation.js
+++ src/components/layout/navigation.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'gatsby';
 import classNames from 'classnames';
+import { Tween } from 'react-gsap';
 
 const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -38,55 +39,61 @@ const Navigation = () => {
 
       <nav className={classNames('navigation', { 'is-open': isOpen })}>
         <ul className="navigation__list">
-          <li className="navigation__item">
-            <Link
-              to="/"
-              className="navigation__link"
-              activeClassName="navigation__link--active"
-            >
-              Home
-            </Link>
-          </li>
+          <Tween
+            from={{ y: '-50', opacity: 0 }}
+            ease="power1.out"
+            stagger={0.2}
+          >
+            <li className="navigation__item">
+              <Link
+                to="/"
+                className="navigation__link"
+                activeClassName="navigation__link--active"
+              >
+                Home
+              </Link>
+            </li>
 
-          <li className="navigation__item">
-            <Link
-              to="/about"
-              className="navigation__link"
-              activeClassName="navigation__link--active"
-            >
-              About
-            </Link>
-          </li>
+            <li className="navigation__item">
+              <Link
+                to="/about"
+                className="navigation__link"
+                activeClassName="navigation__link--active"
+              >
+                About
+              </Link>
+            </li>
 
-          <li className="navigation__item">
-            <Link
-              to="/gallery"
-              className="navigation__link"
-              activeClassName="navigation__link--active"
-            >
-              Gallery
-            </Link>
-          </li>
+            <li className="navigation__item">
+              <Link
+                to="/gallery"
+                className="navigation__link"
+                activeClassName="navigation__link--active"
+              >
+                Gallery
+              </Link>
+            </li>
 
-          <li className="navigation__item">
-            <Link
-              to="/faq"
-              className="navigation__link"
-              activeClassName="navigation__link--active"
-            >
-              FAQ
-            </Link>
-          </li>
+            <li className="navigation__item">
+              <Link
+                to="/faq"
+                className="navigation__link"
+                activeClassName="navigation__link--active"
+              >
+                FAQ
+              </Link>
+            </li>
 
-          <li className="navigation__item">
-            <Link
-              to="/contact"
-              className="navigation__link"
-              activeClassName="navigation__link--active"
-            >
-              Contact
-            </Link>
-          </li>
+            <li className="navigation__item">
+              <Link
+                to="/contact"
+                className="navigation__link"
+                activeClassName="navigation__link--active"
+              >
+                Contact
+              </Link>
+            </li>
+          </Tween>
         </ul>
       </nav>
     </>
