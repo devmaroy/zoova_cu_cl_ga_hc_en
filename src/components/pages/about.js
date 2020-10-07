cba7e6116b5c854b59c502a1aae122377fc698f5
Marek Matejovic <me@devmaroy.cz>
2020-10-07 18:00:35 +0200

    Animation added for about

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Reveal, Tween } from 'react-gsap';
 import Card from '../common/card';
 import cardImage from '../../images/pages/about/event-01.jpg';
 import cardIconUsers from '../../images/icons/users.svg';
@@ -7,84 +8,109 @@ import cardIconHeart from '../../images/icons/heart.svg';
 import cardIconMoney from '../../images/icons/money.svg';
 
 const About = () => {
+  const triggerAnimationRef = useRef();
+  const triggerAnimationCardsRef = useRef();
+
   return (
     <section className="section about divider-space-section">
       <div className="container">
-        <div className="about__inner">
-          <h2 className="about__heading section__heading">
-            Help people along with us
-          </h2>
+        <div className="about__inner" ref={triggerAnimationRef}>
+          <Reveal trigger={triggerAnimationRef.current}>
+            <Tween from={{ y: '-50', opacity: 0 }} ease="power1.out">
+              <h2 className="about__heading section__heading">
+                Help people along with us
+              </h2>
 
-          <div className="about__text section__text">
-            <p>
-              See how many people are already helping and how many sponsors
-              dotated their money to people.
-            </p>
+              <div className="about__text section__text">
+                <p>
+                  See how many people are already helping and how many sponsors
+                  dotated their money to people.
+                </p>
 
-            <p>
-              We also organize a lot of events. The current event is{' '}
-              <strong>A Cup Of Tea With People</strong>
-            </p>
-          </div>
+                <p>
+                  We also organize a lot of events. The current event is{' '}
+                  <strong>A Cup Of Tea With People</strong>
+                </p>
+              </div>
+            </Tween>
+          </Reveal>
 
-          <div className="about__overview section__spacing">
-            <Card
-              featured
-              image={cardImage}
-              date="May 15, 2018"
-              heading="A Cup Of Tea With People"
-            >
-              <p>
-                If you haven&apos;t got any charity in your heart, you have the
-                worst kind of heart trouble.
-              </p>
+          <Reveal trigger={triggerAnimationCardsRef.current}>
+            <div className="about__overview section__spacing">
+              <Tween from={{ opacity: 0 }} ease="power1.out" stagger={0.2}>
+                <div className="about__item">
+                  <Card
+                    featured
+                    image={cardImage}
+                    date="May 15, 2018"
+                    heading="A Cup Of Tea With People"
+                  >
+                    <p>
+                      If you haven&apos;t got any charity in your heart, you
+                      have the worst kind of heart trouble.
+                    </p>
 
-              <p>
-                Charity must become a fundamental state of mind and heart that
-                guides us in all we do.
-              </p>
-            </Card>
+                    <p>
+                      Charity must become a fundamental state of mind and heart
+                      that guides us in all we do.
+                    </p>
+                  </Card>
+                </div>
 
-            <Card
-              icon={cardIconUsers}
-              heading="Participants"
-              headingHighlight="235.154"
-            >
-              <p>
-                See how our foundation grows. With a lot of wonderful and nice
-                people.
-              </p>
-            </Card>
+                <div className="about__item">
+                  <Card
+                    icon={cardIconUsers}
+                    heading="Participants"
+                    headingHighlight="235.154"
+                  >
+                    <p>
+                      See how our foundation grows. With a lot of wonderful and
+                      nice people.
+                    </p>
+                  </Card>
+                </div>
 
-            <Card icon={cardIconEvent} heading="Events" headingHighlight="1046">
-              <p>
-                See how our foundation grows. With a lot of wonderful and nice
-                people.
-              </p>
-            </Card>
+                <div className="about__item">
+                  <Card
+                    icon={cardIconEvent}
+                    heading="Events"
+                    headingHighlight="1046"
+                  >
+                    <p>
+                      See how our foundation grows. With a lot of wonderful and
+                      nice people.
+                    </p>
+                  </Card>
+                </div>
 
-            <Card
-              icon={cardIconHeart}
-              heading="Sponsors"
-              headingHighlight="713"
-            >
-              <p>
-                See how our foundation grows. With a lot of wonderful and nice
-                people.
-              </p>
-            </Card>
+                <div className="about__item">
+                  <Card
+                    icon={cardIconHeart}
+                    heading="Sponsors"
+                    headingHighlight="713"
+                  >
+                    <p>
+                      See how our foundation grows. With a lot of wonderful and
+                      nice people.
+                    </p>
+                  </Card>
+                </div>
 
-            <Card
-              icon={cardIconMoney}
-              heading="Donated"
-              headingHighlight="$5.647.147"
-            >
-              <p>
-                See how our foundation grows. With a lot of wonderful and nice
-                people.
-              </p>
-            </Card>
-          </div>
+                <div className="about__item">
+                  <Card
+                    icon={cardIconMoney}
+                    heading="Donated"
+                    headingHighlight="$5.647.147"
+                  >
+                    <p>
+                      See how our foundation grows. With a lot of wonderful and
+                      nice people.
+                    </p>
+                  </Card>
+                </div>
+              </Tween>
+            </div>
+          </Reveal>
         </div>
       </div>
     </section>
