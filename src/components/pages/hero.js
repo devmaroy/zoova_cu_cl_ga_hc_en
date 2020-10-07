7a98d891ce5455f7a94ae55adb3ec97ebf70bb82
Marek Matejovic <me@devmaroy.cz>
2020-10-07 19:01:13 +0200

    Animation added for hero

--- src/components/pages/hero.js
+++ src/components/pages/hero.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { Tween } from 'react-gsap';
 import heroBg from '../../images/pages/hero/hero-bg-2.jpg';
 
 const Hero = () => {
@@ -9,21 +10,25 @@ const Hero = () => {
       style={{ backgroundImage: `url( ${heroBg} )` }}
     >
       <div className="banner-area__inner hero__inner container">
-        <h1 className="hero__heading">Zoova Foundation</h1>
+        <Tween from={{ y: '-50', opacity: 0 }} ease="power1.out" stagger={0.2}>
+          <h1 className="hero__heading">Zoova Foundation</h1>
 
-        <div className="hero__text">
-          <p>A grateful heart is a beginning of greatness.</p>
-        </div>
+          <div className="hero__text">
+            <p>A grateful heart is a beginning of greatness.</p>
+          </div>
+        </Tween>
 
-        <div className="hero__meta">
-          <Link to="/" className="button button--primary">
-            View gallery
-          </Link>
+        <Tween from={{ opacity: 0 }} ease="power1.out" stagger={0.2}>
+          <div className="hero__meta">
+            <Link to="/" className="button button--primary">
+              View gallery
+            </Link>
 
-          <Link to="/" className="button button--link">
-            Contact us
-          </Link>
-        </div>
+            <Link to="/" className="button button--link">
+              Contact us
+            </Link>
+          </div>
+        </Tween>
       </div>
     </div>
   );
