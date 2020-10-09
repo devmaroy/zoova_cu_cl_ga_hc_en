81ffcb4a6ddf92dde2ea5104db7d5921af779c0b
Marek Matejovic <me@devmaroy.cz>
2020-10-09 10:00:02 +0200

    Animation added for gallery

--- src/components/common/gallery.js
+++ src/components/common/gallery.js
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
-
-// TODO: Change key to ID later.
+import { Reveal, Tween } from 'react-gsap';
 
 const Gallery = ({ items }) => {
-  return (
-    <div className="gallery">
-      {items.map(({ image, heading, headingHighlight }) => (
-        <div key={image} className="gallery__item">
-          <div className="gallery__image-wrapper">
-            <img src={image} className="gallery__image" alt="Gallery item" />
-          </div>
+  const triggerAnimationRef = useRef();
 
-          <h4 className="gallery__heading">
-            <span className="gallery__heading--highlight">
-              {headingHighlight}{' '}
-            </span>
-            {heading}
-          </h4>
-        </div>
-      ))}
+  return (
+    <div className="gallery" ref={triggerAnimationRef}>
+      <Reveal trigger={triggerAnimationRef.current}>
+        <Tween from={{ y: -50, opacity: 0 }} ease="power1.out" stagger={0.2}>
+          {items.map(({ image, heading, headingHighlight }) => (
+            <div key={image} className="gallery__item">
+              <div className="gallery__image-wrapper">
+                <img
+                  src={image}
+                  className="gallery__image"
+                  alt="Gallery item"
+                />
+              </div>
+
+              <h4 className="gallery__heading">
+                <span className="gallery__heading--highlight">
+                  {headingHighlight}{' '}
+                </span>
+
+                {heading}
+              </h4>
+            </div>
+          ))}
+        </Tween>
+      </Reveal>
     </div>
   );
 };
